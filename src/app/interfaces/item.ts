9845afb28aef0a409a4ca6549e2bb3a245c6d072
Alexis Ayala <lex.solorio15@gmail.com>
2024-01-22 19:07:20 -0600

    compiler version only

--- src/app/interfaces/item.ts
+++
@@ -1,6 +0,0 @@
-export interface Item {
-  producto?: string,
-  imagen?: string,
-  precio: number,
-  descripcion?: string
-}
--- src/app/models/coffee.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CoffeeModel } from './coffee.model';
-
-describe('CoffeeModel', () => {
-  it('should create an instance', () => {
-    expect(new CoffeeModel()).toBeTruthy();
-  });
-});
